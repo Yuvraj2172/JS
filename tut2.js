2db92845283efe511c6c5b6d591a2b82377ce0c2
Yuvraj <demonsroar2172@gmail.com>
2022-06-17 14:55:55 +0530

    JS intro

---
+++ tut2.js
@@ -0,0 +1,6 @@
+console.time('Your code took');
+console.log("HII YuVRAJ");
+console.warn('This is a warning');
+// console.clear();
+/*console.timeEnd('Your code took');
+console.error('This is an error');*/ 
