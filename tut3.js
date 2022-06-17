d818dee2ff7f2b4441b662a9f92507936d77a55f
Yuvraj <demonsroar2172@gmail.com>
2022-06-17 14:56:28 +0530

    Variables in JS

---
+++ tut3.js
@@ -0,0 +1,45 @@
+console.log('tut3')
+// variables in JS
+// var ,let const 
+var name ='yuvraj';
+console.log(name);
+
+var channel;
+console.log(name,channel);
+
+var marks =34;
+console.log(name,channel,marks);
+
+var marks;
+console.log(marks);
+
+console.log(marks+10);
+
+var city = `Mathura`;
+console.log(city);
+
+const owner ='Soni';
+console.log(owner);
+
+// owner='uveee'; give errror
+// console.log(owner);
+
+{
+    let city ='Rampur';
+    console.log(city);
+    city='agra'
+    console.log(city)
+}
+console.log(city);
+
+const arr1=[12,23,43,55];
+console.log(arr1);
+
+arr1.push(99);
+console.log(arr1);
+
+arr1.pop(43);
+console.log(arr1);
+
+arr1.fill(1);
+console.log(arr1);
