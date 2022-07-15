107e035e011e55beed18df3c388837b06b9ccf14
yuvraj <demonsroar2172@gmail.com>
2022-07-15 13:37:13 +0530

    modified tut11.js

--- tut11.js
+++ tut11.js
@@ -1,21 +1,15 @@
-console.log('This is tut 11');
-let a = window;
-console.log(a);
-// both will work same 
-// alert('hio')
-// window.alert('hi');
-// a=prompt('This is prompt');
-// a=confirm('Are you sure you want to sumit this form');
-// a=window.document;
-// a=window.innerWidth
+console.log('This is tut11');
+let a = window.document;
+// alert("hello yuvraj");
+// a=prompt("this is dangeros");
+// a=confirm('Are you sure you want to reload');
+// a=window.innerHeight;
+// a=innerWidth;
+// a=outerHeight
+// a=outerWidth
 // a=scrollX;
+
 // a=location.toString();
-a=window.history;
-
-// to go one step forward 
-history.go(1);
-
-// to go one step backward
-history.go(-1);
-
+// a=history;
+// history(-1);
 console.log(a);
