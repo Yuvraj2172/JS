1c5c78c22f9dc3fb7b48612bd2cf9810a172f643
Yuvraj Soni <demonsroar2172@gmail.com>
2023-02-12 16:17:11 +0530

    Variables in JS

---
+++ tut2.js
@@ -0,0 +1,12 @@
+/* author : Yuvraj Soni */
+console.log('This repl contains code for the Ultimate JavScript course video no 2');
+let a = 67; // a contains 67
+console.log(a);
+a = 'yuvraj';
+console.log(a);
+// let 8yuvraj = 67; // Syntax Error...
+
+let Yuvraj = 21;
+let yuvraj = 12;
+console.log(Yuvraj); // 21
+console.log(12); //12
