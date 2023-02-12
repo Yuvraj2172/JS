b2bb7afd07ab2bd46bf8e385058d932835b005df
Yuvraj Soni <demonsroar2172@gmail.com>
2023-02-12 16:10:33 +0530

    Init

--- tut2.js
+++
@@ -1,6 +0,0 @@
-console.time('Your code took');
-console.log("HII YuVRAJ");
-console.warn('This is a warning');
-// console.clear();
-/*console.timeEnd('Your code took');
-console.error('This is an error');*/ 
