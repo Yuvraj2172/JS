51033bc850cb835cdb87637ca8d318336d8e89ec
Yuvraj Soni <demonsroar2172@gmail.com>
2023-02-19 00:50:58 +0530

    Array in JS

---
+++ tut16.js
@@ -0,0 +1,7 @@
+let marks_class_12 = [91 , 82 , 63 , 84];
+let arr = [];
+arr[0] = 123;
+arr[10]= 34;
+console.log(arr[6]);
+console.log("The length of marks_class_12 is " +marks_class_12.length);
+for(let i =0;i<marks_class_12.length;i++)console.log(marks_class_12[i]);
