d803645abffaef6af085892d41daa323e923300c
Yuvraj Soni <demonsroar2172@gmail.com>
2023-02-18 21:20:38 +0530

    Strings

---
+++ tut13.js
@@ -0,0 +1,16 @@
+let name= "Yuvraj Soni";
+console.log(name);
+let name2 = String("Yuvi");
+console.log(name2);
+
+let name3 = `This is modern way to define string`;
+console.log(name3);
+let boy1 = 'Pramod';
+let boy2 = 'Prakash';
+let boy3 = 'saksham';
+let sentence = `${boy1} is a friend of ${boy2} and also a friend of ${boy3}`;
+console.log(sentence);
+
+//Escape Sequence Character
+let fruit ='Bana\"na';
+console.log(fruit);
