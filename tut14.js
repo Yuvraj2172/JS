6727806dc859665fad34720172821e5519a39800
Yuvraj Soni <demonsroar2172@gmail.com>
2023-02-18 23:37:00 +0530

    String Methods

---
+++ tut14.js
@@ -0,0 +1,13 @@
+let name = "yuvraj";
+console.log(name.slice(5));
+console.log(name.replace('a',"aa"));
+let name2="Soni"
+console.log(name.concat("    ",name , name2,"dsfsdfs     ").trim());
+// console.log(name.trim());
+
+let fr = "Shivika";
+
+//String is immutable
+// fr[0] = 'y';
+
+console.log(fr)
