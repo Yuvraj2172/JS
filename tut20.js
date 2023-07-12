c6a4aa3c5b497b137959656bb8896718e9572111
Yuvraj Soni <demonsroar2172@gmail.com>
2023-07-12 10:57:46 +0530

    JS Challenge

---
+++ tut20.js
@@ -0,0 +1,6 @@
+let arr=[45,23,21];
+let a =arr.map((value, index,a)=>{
+   return value+6;
+})
+console.log(a);
+// console.log(arr);
---
+++ udemy/app.js
@@ -0,0 +1,14 @@
+console.log('This is a JS tutorial.')
+// let firstName = "Yuvraj" , lastName = "Soni";
+// let address = "main street";
+// address = "first street";
+// console.log(firstName , lastName , address);
+
+// let street = "mathura" , country = "India";
+// let fullMailingAddress = street + country;
+// console.log(fullMailingAddress);
+
+let score1 = 100, score2=95, score3 = 98;
+let totalScore = (score1+score2+score3);
+let avg = totalScore/3;
+console.log( parseFloat( avg));
